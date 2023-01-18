6103ae4d9f95abe0239d42ed3b082f5aaab1f094
kxrai <alicia.2005.loi@gmail.com>
2023-01-18 15:02:22 -0500

    alicia push

---
+++ src/main/java/com/example/SecondaryController.java
@@ -0,0 +1,27 @@
+package com.example;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+
+public class SecondaryController {
+
+    @FXML
+    private Text PTFtitle;
+
+    @FXML
+    private Text econTitle;
+
+    @FXML
+    private Text genflightTitle;
+
+    @FXML
+    private Button primaryButton;
+
+    @FXML
+    void switchToSecondary(ActionEvent event) {
+
+    }
+
+}
