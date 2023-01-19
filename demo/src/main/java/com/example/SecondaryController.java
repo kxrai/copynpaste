a4de99bd3b1e78ffd4978f729f190c37241f0be4
Davtwo <35241471+Davtwo@users.noreply.github.com>
2023-01-19 00:45:57 -0500

    first screen complete

--- demo/src/main/java/com/example/SecondaryController.java
+++ demo/src/main/java/com/example/SecondaryController.java
@@ -1,18 +1,49 @@
 package com.example;
 
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class SecondaryController {
 
     @FXML
-    private Button primaryButton;
+    private TextField departCode;
 
     @FXML
-    void switchToSecondary(ActionEvent event) {
+    private TextField departDate;
 
+    @FXML
+    private TextField destCode;
+
+    @FXML
+    private TextField returnDate;
+
+    @FXML
+    private Button searchButton;
+
+    @FXML
+    void switchToSecondary(ActionEvent event) throws IOException {
+        String dptCode = departCode.getText();
+        String dptDate = departDate.getText();
+        String dstCode = destCode.getText();
+        String rtrDate = returnDate.getText();
+        ArrayList<String> toPyarr = new ArrayList<String>();
+        toPyarr.add(dptCode);
+        toPyarr.add(dstCode);
+        toPyarr.add(dptDate);
+        toPyarr.add(rtrDate);
+        FileWriter writer = new FileWriter("toPyScraper.txt"); 
+        for(String str: toPyarr) {
+        writer.write(str + System.lineSeparator());
+        }
+        writer.close();
+
+        
     }
+
 }
