ecf3faa831846fec7122fae7cfb4b7f6ec65db97
karunnnn <92546696+karunnnn@users.noreply.github.com>
2023-01-21 01:25:27 -0500

    Update determineTicket.py

--- demo/src/main/java/com/example/determineTicket.py
+++ demo/src/main/java/com/example/determineTicket.py
@@ -64,9 +64,11 @@ with open("toPyScraper.txt", "r") as file:
 for i in range(0, len(lines)):    
     print(lines[i])
 
-distance = float(lines[4])
+distance = int(lines[4])
 departure_date = lines[2]
 return_date = lines[3]
+
+
 
 
 prices = cheapest_days(distance, departure_date, return_date)
